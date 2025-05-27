374eb9d037fdb81f527d41ac5548bad9f15979ab
JinHyeong0709 <wlsgud0709@estsoft.com>
2025-05-27 18:23:26 +0900

    version up to 0.2.0

--- Package.swift
+++ Package.swift
@@ -18,8 +18,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "PersoLiveChatOnDeviceSDK",
-            url: "https://github.com/est-perso-live/perso-livechat-ondevice-sdk-swift/releases/download/0.1.0/PersoLiveChatOnDeviceSDK.xcframework.zip",
-            checksum: "5843d2ca9faf8ae481de55444c044fc1dd46af57e130c1e786ede2ed155e2d82"
+            url: "https://github.com/est-perso-live/perso-livechat-ondevice-sdk-swift/releases/download/0.2.0/PersoLiveChatOnDeviceSDK.xcframework.zip",
+            checksum: "31426d3ae81829cb8aa804646d2ec9cf1c2684352c25c0494059584e0d260c11"
         )
     ]
 )
